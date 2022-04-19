de1cccdbbba5ec4bc608230450f3088af940e519
franco-nahuel-ibanez <franco.nahuel.ibanez@gmail.com>
2022-04-19 11:58:41 -0300

    comentarios eliminados

--- src/rutas/productos.js
+++ src/rutas/productos.js
@@ -2,20 +2,6 @@ const router = require('express').Router();
 const validate = require('../middleware/validate');
 const controladorProductos = require('../controlador/controladorProductos');
 
-// let products = [
-//     {   
-//         id: 1,
-//         title: 'tele',
-//         price: 4500,
-//         thumbnail: 'tele.jpg'
-//     },
-//     {   
-//         id: 2,
-//         title: 'estereo',
-//         price: 1500,
-//         thumbnail: 'estereo.jpg'
-//     }
-// ];
 
 router.get('/:id?', controladorProductos.getProducts)
 router.post('/', validate, controladorProductos.addProducts );
